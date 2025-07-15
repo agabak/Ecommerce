832877b533c800cfd44f41ce26ec9d601b05bd36
agabak <agaba_k@hotmail.com>
2025-07-15 11:31:29 -0500

    refactor

--- Ecom-Client/Program.cs
+++ Ecom-Client/Program.cs
@@ -4,6 +4,7 @@ using Ecom_Client.Components;
 using Ecom_Client.Services;
 using Ecom_Client.Services.https;
 using Ecommerce.Common.Services.Kafka;
+using Ecommerce.Common.Settings.Extension;
 using Ecommerce.Common.Settings.Kafka;
 using Microsoft.Extensions.Options;
 
@@ -25,37 +26,7 @@ builder.Services.AddHttpClient<IAuthService, AuthService>(client =>
 
  builder.Services.AddBlazoredLocalStorage();
 
-// Bind and register ConsumerSettings
-builder.Services.Configure<ConsumerSettings>(
-    builder.Configuration.GetSection("ConsumerSettings"));
-
-// Bind and register ProducerSettings
-builder.Services.Configure<ProducerSettings>(
-    builder.Configuration.GetSection("ProducerSettings"));
-
-// (Optional) Register strongly-typed settings for direct injection
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IOptions<ConsumerSettings>>().Value);
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IOptions<ProducerSettings>>().Value);
-
-// Register Kafka consumer using DI-bound settings
-builder.Services.AddSingleton<IConsumer<string, string>>(sp =>
-{
-    var settings = sp.GetRequiredService<ConsumerSettings>();
-    return new ConsumerBuilder<string, string>(settings).Build();
-});
-
-// Register Kafka producer using DI-bound settings
-builder.Services.AddSingleton<IProducer<string, string>>(sp =>
-{
-    var settings = sp.GetRequiredService<ProducerSettings>();
-    return new ProducerBuilder<string, string>(settings).Build();
-});
-
-builder.Services.AddSingleton<IProducerService, ProducerService>();
-builder.Services.AddSingleton<IConsumerService, ConsumerService>();
-
+builder.Services.AddKafkaConsumerProducer(builder.Configuration);
 
 builder.Services.AddSingleton<ICartService, CartService>();
 
--- Ecom-OderWorkerService/Program.cs
+++ Ecom-OderWorkerService/Program.cs
@@ -3,6 +3,7 @@ using Ecom_OderWorkerService;
 using Ecom_OderWorkerService.Repositories;
 using Ecom_OderWorkerService.Services;
 using Ecommerce.Common.Services.Kafka;
+using Ecommerce.Common.Settings.Extension;
 using Ecommerce.Common.Settings.Kafka;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Options;
@@ -14,36 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IDbConnection>
     (_ => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"))); // Updated to use Microsoft.Data.SqlClient.SqlConnection
 
-// Bind and register ConsumerSettings
-builder.Services.Configure<ConsumerSettings>(
-    builder.Configuration.GetSection("ConsumerSettings"));
-
-// Bind and register ProducerSettings
-builder.Services.Configure<ProducerSettings>(
-    builder.Configuration.GetSection("ProducerSettings"));
-
-// (Optional) Register strongly-typed settings for direct injection
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IOptions<ConsumerSettings>>().Value);
-builder.Services.AddSingleton(sp =>
-    sp.GetRequiredService<IOptions<ProducerSettings>>().Value);
-
-// Register Kafka consumer using DI-bound settings
-builder.Services.AddSingleton<IConsumer<string, string>>(sp =>
-{
-    var settings = sp.GetRequiredService<ConsumerSettings>();
-    return new ConsumerBuilder<string, string>(settings).Build();
-});
-
-// Register Kafka producer using DI-bound settings
-builder.Services.AddSingleton<IProducer<string, string>>(sp =>
-{
-    var settings = sp.GetRequiredService<ProducerSettings>();
-    return new ProducerBuilder<string, string>(settings).Build();
-});
-
-builder.Services.AddSingleton<IProducerService, ProducerService>();
-builder.Services.AddSingleton<IConsumerService, ConsumerService>();
+builder.Services.AddKafkaConsumerProducer(builder.Configuration);
 
 // Add other services as needed
 builder.Services.AddHostedService<CreateOrderBackgroundService>();
